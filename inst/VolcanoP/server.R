0ffe9bdde0944527eb9e63f5abe50c48525c08d7
likelet <zhaoqi699@sina.com>
2017-06-17 01:02:23 +0800

    first commit

---
+++ inst/VolcanoP/server.R
@@ -0,0 +1,186 @@
+# Must be executed BEFORE rgl is loaded on headless devices.
+options(rgl.useNULL=TRUE)
+
+library(shiny)
+library(ggplot2)
+library(ggthemes)
+library(dplyr)
+library(ggrepel)
+#library(Cairo)
+
+
+#x denote dataframe,y denote samplenames
+
+
+
+shinyServer(function(input,output,session){
+   
+  textinput<-reactive({
+    a<-input$markered
+    a<-as.character(unlist(strsplit(a,"\n")))
+    a
+  })
+  
+  
+    datasetInput <- reactive({ 
+                                
+          example<-read.table("data/test.txt",header=T,sep="\t",row.names=1)
+          #example<-read.table("/srv/shiny-server/vocalnoPlotOnline/test.txt",header=T,sep="\t",row.names=1)
+           	inFile <- input$file1
+            if (!is.null(inFile)){
+                 
+                data<-read.table(inFile$datapath, header=input$header, sep=input$sep, quote=input$quote,row.names=1) 		
+             
+            }
+            
+           switch(input$dataset,
+                    "example" = example,
+                    "upload" = data
+                    )	
+    	})
+    
+  
+    
+    #get plotdata
+    plotdataInput<-reactive({
+      a<-datasetInput()
+      P.Value <- a[,2]
+      FC<-a[,1]
+      df<-data.frame(P.Value, FC)
+      
+      df$gene=row.names(a)
+      
+      # df$threshold<-as.factor(abs(df$FC) > input$FCcut & df$P.Value < input$pcut)
+      # df
+      # 
+      
+      df=mutate(df,threshold = ifelse(df$P.Value >input$pcut, 
+                              yes = "none", 
+                              no = ifelse(df$FC < 0, 
+                                          yes = "Down-regulated", 
+                                          no = "Up-regulated")))
+      df
+      
+    })
+    
+  
+    
+# output upload dataset or example dataset  
+    output$summary <- DT::renderDataTable(plotdataInput())
+ #plotfunction
+    getvocalnoPlot<-reactive({
+      df<-plotdataInput()
+      g <- ggplot(data=df, aes(x=FC, y=-log10(P.Value), colour=factor(threshold))) +
+        geom_point( size=1.75) +
+        ylim(c(0, input$ylmslider)) +
+        xlab("log2 fold change") + ylab("-log10 p-value")+
+        scale_y_continuous(trans = "log1p")+
+        geom_vline(xintercept = 0, colour = "black") + # add line at 0
+        geom_hline(yintercept = -log10(input$pcut), colour = "black")  # p(0.05) = 1.3
+      if(input$theme=="default"){
+      g=g+theme(legend.position = "none")+ theme_bw()+scale_color_manual(values = c("Down-regulated" = "#E64B35", 
+                                                                                    "Up-regulated" = "#3182bd", 
+                                                                                    "none" = "#636363"))
+      }else if(input$theme=="Tufte"){
+      g=g+geom_rangeframe() + theme_tufte()
+      }else if(input$theme=="Economist"){
+        g=g+ theme_economist()+ scale_colour_economist()
+      }else if(input$theme=="Solarized"){
+        g=g+ theme_solarized()+ scale_colour_solarized("blue")
+      }else if(input$theme=="Stata"){
+        g=g+ theme_stata() + scale_colour_stata()
+      }else if(input$theme=="Excel 2003"){
+        g=g+ theme_excel() + scale_colour_excel()
+      }else if(input$theme=="Inverse Gray"){
+        g=g+ theme_igray()
+      }else if(input$theme=="Fivethirtyeight"){
+        g=g+scale_color_fivethirtyeight()+ theme_fivethirtyeight()
+      }else if(input$theme=="Tableau"){
+        g=g+theme_igray()+ scale_colour_tableau()
+      }else if(input$theme=="Stephen"){
+        g=g+theme_few()+ scale_colour_few()
+      }else if(input$theme=="Wall Street"){
+        g=g+theme_wsj()+ scale_colour_wsj("colors6", "")
+      }else if(input$theme=="GDocs"){
+        g=g+theme_gdocs()+ scale_color_gdocs()
+      }else if(input$theme=="Calc"){
+        g=g+theme_calc()+ scale_color_calc()
+      }else if(input$theme=="Pander"){
+        g=g+theme_pander()+ scale_colour_pander()
+      }else if(input$theme=="Highcharts"){
+        g=g+theme_hc()+ scale_colour_hc()
+      }
+      if(input$Annotate){
+        markedname=textinput()
+        g=g+geom_text_repel(data=filter(df, gene%in%markedname), aes(label=gene))
+      }
+      return(g)
+    })
+
+    output$vocalnoPlot<-renderPlot({
+      g<-getvocalnoPlot()
+      print(g)  
+      },height=600,width="auto")
+    
+    
+    
+
+  
+
+    
+#download plot option    
+    output$downloadDataPNG <- downloadHandler(
+      filename = function() {
+        paste("output", Sys.time(), '.png', sep='')
+      },
+      
+      content = function(file) {
+        ggsave(file, getvocalnoPlot(),width = 10, height = 10, units = "in",pointsize=5.2)
+      },
+      contentType = 'image/png'
+    )
+    
+    
+    output$downloadDataPDF <- downloadHandler(
+      filename = function() {
+        paste("output", Sys.time(), '.pdf', sep='')
+      },
+      
+      content = function(file) {
+        ggsave(file, getvocalnoPlot(),width = 10, height = 10, units = "in",pointsize=5.2)
+      },
+      contentType = 'image/pdf'
+    )
+    
+    output$downloadDataEPS <- downloadHandler(
+      filename = function() {
+        paste("output", Sys.time(), '.eps', sep='')
+      },
+      
+      content = function(file) {
+        ggsave(file, getvocalnoPlot(),width = 10, height = 10, units = "in",pointsize=5.2)
+      },
+      contentType = 'image/eps'
+    )
+    
+    output$downloadDataTIFF <- downloadHandler(
+      filename = function() {
+        paste("output", Sys.time(), '.tiff', sep='')
+      },
+      
+      content = function(file) {
+        ggsave(file, getvocalnoPlot(),width = 10, height = 10, units = "in",pointsize=5.2)
+      },
+      contentType = 'image/eps'
+    )
+
+
+
+
+
+
+
+})
+
+
+
---
+++ inst/VolcanoP/ui.R
@@ -0,0 +1,105 @@
+library(shiny)
+library(shinydashboard)
+library(DT)
+library(shinyBS)
+#difined a text area input
+inputTextarea <- function(inputId, value="", nrows, ncols) {
+  tagList(
+    singleton(tags$head(tags$script(src = "textarea.js"))),
+    tags$textarea(id = inputId,
+                  class = "inputtextarea",
+                  rows = nrows,
+                  cols = ncols,
+                  as.character(value))
+  )
+}
+
+
+dashboardPage(
+  dashboardHeader(title = "Vocalno Plot Online"),
+  dashboardSidebar(
+    tags$link(rel="stylesheet",type="text/css",href="main.css"),
+    sidebarMenu(
+    menuItem("ReadMe", tabName = "read", icon = icon("dashboard"))
+    )
+  ),
+  dashboardBody(
+    # First tab content
+    tabItem("read",
+            fluidRow(
+              box(
+                width = 4, status = "info", solidHeader = TRUE,collapsible = TRUE,
+                title = "Data Input",
+                radioButtons("dataset", "", c(Example = "example", Upload = "upload",Input="Inputlist"),selected = 'example'),
+                conditionalPanel(
+                  condition = "input.dataset == 'upload'",
+
+                  fileInput('file1', 'Choose CSV/text File',
+                            accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
+                  checkboxInput('header', 'Header', FALSE),
+                  radioButtons('sep', 'Separator',
+                               c(Comma=',',
+                                 Semicolon=';',
+                                 Tab='\t'),
+                               '\t'),
+                  radioButtons('quote', 'Quote',
+                               c(None='',
+                                 'Double Quote'='"',
+                                 'Single Quote'="'"),
+                               '')
+                ),
+
+
+                  p("A volcano plot displays unstandardized signal (e.g. log-fold-change) against noise-adjusted/standardized signal (e.g. t-statistic or -log(10)(p-value) from statistic test(t-test,anova,etc)",
+                    br(),
+                    br(),
+                    strong("This application was created by "),
+                    tags$a(href="mailto:zhaoqi3@mail2.sysu.edu.cn",strong("Qi Zhao")),
+                    strong(' from '),
+                    tags$a(href="http://gps.biocuckoo.org/",strong("Ren Lab")),
+                    strong(" in "),
+                    tags$a(href="http://www.sysu.edu.cn/2012/en/index.htm",strong("SYSU")),
+                    strong('. Please let us know if you find bugs or have new feature request.This application uses the'),
+                    tags$a(href="http://www.rstudio.com/shiny/",strong("shiny package from RStudio."))
+                  ),
+                tags$button(id="goPlot",type="button",class="btn btn-primary action-button","GO PLOT!")
+                ),
+                box(width = 8, status = "success",solidHeader = TRUE,collapsible = TRUE,
+                  title = "Data Summary",
+                    DT::dataTableOutput("summary")
+              ),
+
+              bsModal("KEGGPlotModal", h3("Fancy Plot"), "goPlot", size = "large",
+                      box(title = "Controls", width = 3, status = "info", solidHeader = TRUE,collapsible = TRUE,
+                          numericInput("pcut",strong("P.Value threshold"),0.05),
+                          numericInput("FCcut",strong("Fold change threshold"),1),
+                          sliderInput("xlmslider", strong("Xlim range"), 1, 10, 5, step = 0.5,animate = TRUE),
+                          sliderInput("ylmslider", strong("ylim range"), 1, 50, 5, step = 0.5,animate = TRUE),
+                          selectInput("theme", "Plot Theme:",
+                                      c("default","Tufte","Economist","Solarized","Stata","Excel 2003","Inverse Gray","Fivethirtyeight","Tableau","Stephen","Wall Street","GDocs","Calc","Pander","Highcharts")),
+
+                          checkboxInput('Annotate', 'Annotate', FALSE),
+                          conditionalPanel(
+                            condition = "input.Annotate == true",
+                            h4("Marked label"),
+                            inputTextarea("markered","ENSDARG00000088353\nENSDARG00000079727\nENSDARG00000079727",10,10)
+                          )
+                      ),
+                      box(title = "Volcano", width = 9, status = "success", solidHeader = TRUE,collapsible = TRUE,
+                      div(
+                        downloadLink('downloadDataPNG', 'Download PNG-file',class="downloadLinkblack"),
+                        downloadLink('downloadDataPDF', 'Download PDF-file',class="downloadLinkred"),
+                        downloadLink('downloadDataEPS', 'Download EPS-file',class="downloadLinkblue"),
+                        downloadLink('downloadDataTIFF', 'Download TIFF-file',class="downloadLinkgreen")
+                        ,
+                        plotOutput("vocalnoPlot",height="100%",width="100%")
+                      )
+                      )
+
+
+              )
+
+            )
+    )
+  )
+)
